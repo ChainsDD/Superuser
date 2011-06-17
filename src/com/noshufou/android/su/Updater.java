93b7c907fac8f78fdd2977ad391e2ebf892d1b35
Adam Shanks <chainsdd@gmail.com>
2011-06-17 18:26:39 +0100

    Fix for updater cconstantly thinking there's a new version of the binary.

--- src/com/noshufou/android/su/Updater.java
+++ src/com/noshufou/android/su/Updater.java
@@ -62,8 +62,8 @@ public class Updater {
                 expectedSuVersion += "-cd";
                 mBinaries = mManifest.getJSONObject("cd");
             } else {
-                expectedSuVersion += "-efg";
-                mBinaries = mManifest.getJSONObject("efg");
+                expectedSuVersion += "-efgh";
+                mBinaries = mManifest.getJSONObject("efgh");
             }
             if (mSuVersion == null || !mSuVersion.equals(expectedSuVersion)) {
                 Log.d(TAG, "System has outdated su, attempting to copy new version");
