8714d073b1b13e42d7b635f4291b2f5cab3fb204
Adam Shanks <chainsdd@gmail.com>
2011-09-26 04:14:58 +0200

    Edited manifest.js via GitHub

--- manifest.js
+++ manifest.js
@@ -4,8 +4,26 @@
     "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DNKPJ3PX44ZU8",
     "roms": [
         {
+            "name": "Superuser 3.0 d",
+            "summary": "Donut",
+            "device": "all",
+            "urls": [
+                "http://goo-inside.me/superuser/su-3.0-d-signed.zip",
+                "http://downloads.androidsu.com/superuser/su-3.0-d-signed.zip"
+            ]
+        },
+        {
+            "name": "Superuser 3.0 efgh",
+            "summary": "Eclair, Froyo, Gingerbread, and Honeycomb",
+            "device": "all",
+            "urls": [
+                "http://goo-inside.me/superuser/su-3.0-efgh-signed.zip",
+                "http://downloads.androidsu.com/superuser/su-3.0-efgh-signed.zip"
+            ]
+        },
+        {
             "name": "Superuser 2.3.6.3 cd",
-            "summary": "Cupcake and Donut",
+            "summary": "Cupcake and Donut (no longer supported)",
             "device": "all",
             "urls": [
                 "http://goo-inside.me/superuser/su-2.3.6.3-cd-signed.zip",
@@ -14,29 +32,11 @@
         },
         {
             "name": "Superuser 2.3.6.3 efgh",
-            "summary": "Eclair, Froyo, Gingerbread, and Honeycomb",
+            "summary": "Eclair, Froyo, Gingerbread, and Honeycomb (no longer supported)",
             "device": "all",
             "urls": [
                 "http://goo-inside.me/superuser/su-2.3.6.3-efgh-signed.zip",
                 "http://downloads.androidsu.com/superuser/su-2.3.6.3-efgh-signed.zip"
-            ]
-        },
-        {
-            "name": "Superuser 3.0 Beta 4 cd",
-            "summary": "Cupcake and Donut",
-            "device": "all",
-            "urls": [
-                "http://goo-inside.me/superuser/su-3.0-beta4-cd-signed.zip",
-                "http://downloads.androidsu.com/superuser/su-3.0-beta4-cd-signed.zip"
-            ]
-        },
-        {
-            "name": "Superuser 3.0 Beta 4 efgh",
-            "summary": "Eclair, Froyo, Gingerbread, and Honeycomb",
-            "device": "all",
-            "urls": [
-                "http://goo-inside.me/superuser/su-3.0-beta4-efgh-signed.zip",
-                "http://downloads.androidsu.com/superuser/su-3.0-beta4-efgh-signed.zip"
             ]
         }
     ]
