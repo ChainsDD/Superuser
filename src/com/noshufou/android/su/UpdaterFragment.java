339578ac221425c2b9995c746a4dfbccb59105f4
Adam Shanks <chainsdd@gmail.com>
2011-09-30 13:41:58 +0100

    Only change the file mode once, fix commit fail

--- src/com/noshufou/android/su/UpdaterFragment.java
+++ src/com/noshufou/android/su/UpdaterFragment.java
@@ -450,19 +450,6 @@ public class UpdaterFragment extends ListFragment implements OnClickListener {
                     publishProgress(progressTotal, progressStep, progressStep,
                             R.string.updater_ok, CONSOLE_GREEN);
                     
-                    // Change su file mode
-                    progressStep++;
-                    publishProgress(progressTotal, progressStep - 1, progressStep,
-                            R.string.updater_step_chmod);
-                    inLine = executeCommand(os, is, mBusyboxPath, "chmod 06755", installedSu, "&&",
-                            mBusyboxPath, "echo YEAH");
-                    if (inLine == null || !inLine.equals("YEAH")) {
-                        publishProgress(progressTotal, progressStep - 1, progressStep,
-                                R.string.updater_fail, CONSOLE_RED);
-                    }
-                    publishProgress(progressTotal, progressStep, progressStep,
-                            R.string.updater_ok, CONSOLE_GREEN);
-
                     // Remount system partition
                     progressStep++;
                     publishProgress(progressTotal, progressStep - 1, progressStep,
