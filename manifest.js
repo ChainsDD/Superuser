af40bb598a39d7d257487647c45b13a4e4cdad50
Denham Crafton <drmacinyasha@gmail.com>
2011-10-11 12:31:56 -0700

    Superuser 3.0.5

--- manifest.js
+++ manifest.js
@@ -4,21 +4,21 @@
     "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DNKPJ3PX44ZU8",
     "roms": [
         {
-            "name": "Superuser 3.0.4 d",
+            "name": "Superuser 3.0.5 d",
             "summary": "Donut",
             "device": "all",
             "urls": [
-                "http://goo-inside.me/superuser/su-3.0.4-d-signed.zip",
-                "http://downloads.androidsu.com/superuser/su-3.0.4-d-signed.zip"
+                "http://goo-inside.me/superuser/su-3.0.5-d-signed.zip",
+                "http://downloads.androidsu.com/superuser/su-3.0.5-d-signed.zip"
             ]
         },
         {
-            "name": "Superuser 3.0.4 efgh",
+            "name": "Superuser 3.0.5 efgh",
             "summary": "Eclair, Froyo, Gingerbread, and Honeycomb",
             "device": "all",
             "urls": [
-                "http://goo-inside.me/superuser/su-3.0.4-efgh-signed.zip",
-                "http://downloads.androidsu.com/superuser/su-3.0.4-efgh-signed.zip"
+                "http://goo-inside.me/superuser/su-3.0.5-efgh-signed.zip",
+                "http://downloads.androidsu.com/superuser/su-3.0.5-efgh-signed.zip"
             ]
         },
         {
