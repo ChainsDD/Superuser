05de022bafc1774029fce6285348362098a5667b
Adam Shanks <chainsdd@gmail.com>
2011-10-22 23:00:30 +0100

    Superuser 3.0.6 and su binary 3.0.1

--- manifest.js
+++ manifest.js
@@ -4,21 +4,39 @@
     "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DNKPJ3PX44ZU8",
     "roms": [
         {
-            "name": "Superuser 3.0.5 d",
+            "name": "Superuser 3.0.6 d",
             "summary": "Donut",
             "device": "all",
             "urls": [
-                "http://goo-inside.me/superuser/su-3.0.5-d-signed.zip",
-                "http://downloads.androidsu.com/superuser/su-3.0.5-d-signed.zip"
+                "http://goo-inside.me/superuser/Superuser-3.0.6-d-signed.zip",
+                "http://downloads.androidsu.com/superuser/Superuser-3.0.6-d-signed.zip"
             ]
         },
         {
-            "name": "Superuser 3.0.5 efgh",
+            "name": "su binary only 3.0.1 d",
+            "summary": "Donut",
+            "device": "all",
+            "urls": [
+                "http://goo-inside.me/superuser/su-3.0.1-bin-d-signed.zip",
+                "http://downloads.androidsu.com/superuser/su-3.0.1-bin-d-signed.zip"
+            ]
+        },
+        {
+            "name": "Superuser 3.0.6 efgh",
             "summary": "Eclair, Froyo, Gingerbread, and Honeycomb",
             "device": "all",
             "urls": [
-                "http://goo-inside.me/superuser/su-3.0.5-efgh-signed.zip",
-                "http://downloads.androidsu.com/superuser/su-3.0.5-efgh-signed.zip"
+                "http://goo-inside.me/superuser/Superuser-3.0.6-efgh-signed.zip",
+                "http://downloads.androidsu.com/superuser/Superuser-3.0.6-efgh-signed.zip"
+            ]
+        },
+        {
+            "name": "su binary only 3.0.1 efgh",
+            "summary": "Eclair, Froyo, Gingerbread, Honeycomb",
+            "device": "all",
+            "urls": [
+                "http://goo-inside.me/superuser/su-3.0.1-bin-efgh-signed.zip",
+                "http://downloads.androidsu.com/superuser/su-3.0.1-bin-efgh-signed.zip"
             ]
         },
         {
