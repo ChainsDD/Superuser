6bf4d09ef21a615759144345c627bf719f496846
Adam Shanks <chainsdd@gmail.com>
2011-11-23 20:25:56 +0000

    Finish fixing build on ICS

--- Android.mk
+++ Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_STATIC_JAVA_LIBRARIES := libarity android-support-v4
 LOCAL_PACKAGE_NAME := Superuser
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 
