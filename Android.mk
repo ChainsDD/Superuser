549178d32d8e69f9bb11ca8f68c44a78bdf29cc5
Adam Shanks <chainsdd@gmail.com>
2011-11-23 21:08:50 +0000

    Sign apk with the proper key

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := libarity android-support-v4
 LOCAL_PACKAGE_NAME := Superuser
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 
-LOCAL_CERTIFICATE := platform
+LOCAL_CERTIFICATE := superuser
 
 include $(BUILD_PACKAGE)
 
