26cb5a3d19669953e181b1ff4661bcab0c201704
Denham Crafton <drmacinyasha@gmail.com>
2011-12-18 10:29:00 -0800

    Reorder downloads to place more common files at top

--- manifest.js
+++ manifest.js
@@ -4,6 +4,24 @@
     "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DNKPJ3PX44ZU8",
     "roms": [
         {
+            "name": "Superuser 3.0.7 efghi",
+            "summary": "Eclair, Froyo, Gingerbread, Honeycomb, and Ice Cream Sandwich",
+            "device": "all",
+            "urls": [
+                "http://goo-inside.me/superuser/Superuser-3.0.7-efghi-signed.zip",
+                "http://downloads.androidsu.com/superuser/Superuser-3.0.7-efghi-signed.zip"
+            ]
+        },
+        {
+            "name": "su binary-only 3.0.3.2 efghi",
+            "summary": "Eclair, Froyo, Gingerbread, Honeycomb, and Ice Cream Sandwich",
+            "device": "all",
+            "urls": [
+                "http://goo-inside.me/superuser/su-bin-3.0.3.2-efghi-signed.zip",
+                "http://downloads.androidsu.com/superuser/su-bin-3.0.3.2-efghi-signed.zip"
+            ]
+        },
+        {
             "name": "Superuser 3.0.7 d",
             "summary": "Donut",
             "device": "all",
@@ -13,39 +31,12 @@
             ]
         },
         {
-            "name": "su binary only 3.0.3.2 d",
+            "name": "su binary-only 3.0.3.2 d",
             "summary": "Donut",
             "device": "all",
             "urls": [
                 "http://goo-inside.me/superuser/su-bin-3.0.3.2-d-signed.zip",
                 "http://downloads.androidsu.com/superuser/su-bin-3.0.3.2-d-signed.zip"
-            ]
-        },
-        {
-            "name": "Superuser 3.0.7 efghi",
-            "summary": "Eclair, Froyo, Gingerbread, and Honeycomb",
-            "device": "all",
-            "urls": [
-                "http://goo-inside.me/superuser/Superuser-3.0.7-efghi-signed.zip",
-                "http://downloads.androidsu.com/superuser/Superuser-3.0.7-efghi-signed.zip"
-            ]
-        },
-        {
-            "name": "su binary only 3.0.3.2 efghi",
-            "summary": "Eclair, Froyo, Gingerbread, Honeycomb",
-            "device": "all",
-            "urls": [
-                "http://goo-inside.me/superuser/su-bin-3.0.3.2-efghi-signed.zip",
-                "http://downloads.androidsu.com/superuser/su-bin-3.0.3.2-efghi-signed.zip"
-            ]
-        },
-        {
-            "name": "Superuser 2.3.6.3 cd",
-            "summary": "Cupcake and Donut (no longer supported)",
-            "device": "all",
-            "urls": [
-                "http://goo-inside.me/superuser/su-2.3.6.3-cd-signed.zip",
-                "http://downloads.androidsu.com/superuser/su-2.3.6.3-cd-signed.zip"
             ]
         },
         {
@@ -57,5 +48,14 @@
                 "http://downloads.androidsu.com/superuser/su-2.3.6.3-efgh-signed.zip"
             ]
         }
+        {
+            "name": "Superuser 2.3.6.3 cd",
+            "summary": "Cupcake and Donut (no longer supported)",
+            "device": "all",
+            "urls": [
+                "http://goo-inside.me/superuser/su-2.3.6.3-cd-signed.zip",
+                "http://downloads.androidsu.com/superuser/su-2.3.6.3-cd-signed.zip"
+            ]
+        },
     ]
 }
