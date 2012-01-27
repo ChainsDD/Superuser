7b69d7e71684fd732532f5efb64ee5acc1421dc7
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-01-27 23:44:28 +0700

    Superuser: Fix FC when checking for su binary update
    Proguard strips out UpdaterFragment because it was never referenced
    from code. This patch adds a proguard flag file to prevent that.
    
    Change-Id: I8d884d81cb6729a46a2dcaf0da1a68dbb4eef651

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_STATIC_JAVA_LIBRARIES := libarity android-support-v4
 LOCAL_PACKAGE_NAME := Superuser
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_CERTIFICATE := superuser
 
