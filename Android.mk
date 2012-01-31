6c5bff0ade382087a7e6776a5f6a9196fa4e3058
Gavin Chapman <gavin.chapman@gmail.com>
2012-01-31 17:34:07 +0000

    Fix superuser build to be same as market version
    Change-Id: I42a57e29ee4cec0a55582f209aa7e7936216cd7e

--- Android.mk
+++ Android.mk
@@ -5,6 +5,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := libarity android-support-v4
 LOCAL_PACKAGE_NAME := Superuser
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+
 LOCAL_CERTIFICATE := superuser
 
 include $(BUILD_PACKAGE)
