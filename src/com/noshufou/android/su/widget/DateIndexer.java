ac274f0c06260ede148579504a35a82918bede9f
Adam Shanks <chainsdd@gmail.com>
2012-02-12 14:29:47 +0000

    Fix ANR caused by faulty date calculation in DateIndexer

--- src/com/noshufou/android/su/widget/DateIndexer.java
+++ src/com/noshufou/android/su/widget/DateIndexer.java
@@ -1,17 +1,17 @@
 package com.noshufou.android.su.widget;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.util.Log;
 import android.widget.SectionIndexer;
 
 import com.noshufou.android.su.util.Util;
 
 public class DateIndexer implements SectionIndexer {
-    private static final String TAG = "Su.DateIndexer";
+//    private static final String TAG = "Su.DateIndexer";
     
     private Cursor mCursor;
     private int mColumnIndex;
@@ -32,9 +32,11 @@ public class DateIndexer implements SectionIndexer {
         mCursor.moveToLast();
         long lastDateLong = mCursor.getLong(mColumnIndex);
 
-        int firstDateInt = Integer.parseInt(mIntFormat.format(firstDateLong));
-        int lastDateInt = Integer.parseInt(mIntFormat.format(lastDateLong));
-        mSectionCount = (firstDateInt - lastDateInt) + 1;
+        Calendar startDate = Calendar.getInstance();
+        startDate.setTimeInMillis(lastDateLong);
+        Calendar endDate = Calendar.getInstance();
+        endDate.setTimeInMillis(firstDateLong);
+        mSectionCount = daysBetween(startDate, endDate);
 
         mSections = new String[mSectionCount];
         mSectionDates = new int[mSectionCount];
@@ -64,10 +66,10 @@ public class DateIndexer implements SectionIndexer {
         }
         
         if (mSectionPositions[section] > 0) {
-            return mSectionPositions[section];
+        	return mSectionPositions[section];
         }
         
-        int start = 0;
+        int start = section;
         int end = mCursor.getCount();
         
         for (int i = section - 1; i > 0; i--) {
@@ -80,37 +82,36 @@ public class DateIndexer implements SectionIndexer {
         int savedCursorPos = mCursor.getPosition();
         long date;
         int dateInt;
+        int result = 0;
         for (int i = start; i < end; i++) {
             if (mCursor.moveToPosition(i)) {
                 date = mCursor.getLong(mColumnIndex);
                 dateInt = Integer.parseInt(mIntFormat.format(date));
                 if (mSectionDates[section] >= dateInt) {
                     mSectionPositions[section] = i;
-                    return i;
+                    result = i;
+                    break;
                 }
             }
         }
         mCursor.moveToPosition(savedCursorPos);
-                
-        return 0;
+        return result;
     }
 
     @Override
     public int getSectionForPosition(int position) {
+        // Find the date for the position
         int savedCursorPos = mCursor.getPosition();
         mCursor.moveToPosition(position);
         long date = mCursor.getLong(mColumnIndex);
         mCursor.moveToPosition(savedCursorPos);
+
         int dateInt = Integer.parseInt(mIntFormat.format(date));
-        // Simple linear search since there aren't that many sections.
-        // May optimize this later if necessary.
         for (int i = 0; i < mSectionCount; i++) {
-            if (dateInt == mSectionDates[i]) {
-                return i;
-            }
+        	if (dateInt == mSectionDates[i]) {
+        		return i;
+        	}
         }
-        // If it wasn't found, something went very wrong. Log it
-        Log.e(TAG, "Section not found for date " + dateInt);
         return 0;
     }
 
@@ -118,5 +119,51 @@ public class DateIndexer implements SectionIndexer {
     public Object[] getSections() {
         return mSections;
     }
+    
+    public static int daysBetween(final Calendar startDate, final Calendar endDate) {
+    	Calendar sDate = (Calendar) startDate.clone();
+    	int daysBetween = 0;
+    	
+    	int y1 = sDate.get(Calendar.YEAR);
+    	int y2 = endDate.get(Calendar.YEAR);
+    	int m1 = sDate.get(Calendar.MONTH);
+    	int m2 = endDate.get(Calendar.MONTH);
+    	
+    	// Year optimization
+    	while (((y2 - y1) * 12 + (m2 - m1)) > 12) {
+    		
+    		// Move to Jan 01
+    		if (sDate.get(Calendar.MONTH) == Calendar.JANUARY
+    				&& sDate.get(Calendar.DAY_OF_MONTH) ==
+    					sDate.getActualMinimum(Calendar.DAY_OF_MONTH)) {
+    			daysBetween += sDate.getActualMaximum(Calendar.DAY_OF_YEAR);
+    			sDate.add(Calendar.YEAR, 1);
+    		} else {
+    			int diff = 1 + sDate.getActualMaximum(Calendar.DAY_OF_YEAR)
+    					- sDate.get(Calendar.DAY_OF_YEAR);
+    			sDate.add(Calendar.DAY_OF_YEAR, diff);
+    			daysBetween += diff;
+    		}
+    		y1 = sDate.get(Calendar.YEAR);
+    	}
+    	
+    	// Month optimization
+    	while ((m2 - m1) % 12 > 1) {
+    		daysBetween += sDate.getActualMaximum(Calendar.DAY_OF_MONTH);
+    		sDate.add(Calendar.MONTH, 1);
+    		m1 = sDate.get(Calendar.MONTH);
+    	}
 
+    	// process the remainder date
+    	while (sDate.before(endDate)) {
+    		sDate.add(Calendar.DAY_OF_MONTH, 1);
+    		daysBetween++;
+    	}
+    	
+    	// Add one more day to include the end date
+    	daysBetween++;
+
+    	return daysBetween;
+    }
+    
 }
