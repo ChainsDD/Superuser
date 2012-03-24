eadcccd1f358f60cdd139fdd848e2fd959a12fab
Adam Shanks <chainsdd@gmail.com>
2012-03-24 10:09:01 +0000

    Revert "last time"
    This reverts commit 76350c4cc87267bd44b81a8b07c4aa9fabed4a14.

--- manifest.js
+++ manifest.js
@@ -8,72 +8,48 @@
             "summary": "Eclair, Froyo, Gingerbread, Honeycomb, and Ice Cream Sandwich",
             "device": "all",
 <<<<<<< HEAD
-<<<<<<< HEAD
             "url": "http://downloads.noshufou.netdna-cdn.com/superuser/Superuser-3.0.7-efghi-signed.zip"
 =======
-=======
-            "url": "http://downloads.androidsu.com/superuser/Superuser-3.0.7-efghi-signed.zip"
->>>>>>> 709922b412684a35369a49b6832bed2219630776
         },
         {
             "name": "su binary-only 3.0.3.2 efghi",
             "summary": "Eclair, Froyo, Gingerbread, Honeycomb, and Ice Cream Sandwich",
             "device": "all",
 <<<<<<< HEAD
-<<<<<<< HEAD
             "url": "http://downloads.noshufou.netdna-cdn.com/superuser/su-bin-3.0.3.2-efghi-signed.zip"
 =======
-=======
-            "url": "http://downloads.androidsu.com/superuser/su-bin-3.0.3.2-efghi-signed.zip"
->>>>>>> 709922b412684a35369a49b6832bed2219630776
         },
         {
             "name": "Superuser 3.0.7 d",
             "summary": "Donut",
             "device": "all",
 <<<<<<< HEAD
-<<<<<<< HEAD
             "url": "http://downloads.noshufou.netdna-cdn.com/superuser/Superuser-3.0.7-d-signed.zip"
 =======
-=======
-            "url": "http://downloads.androidsu.com/superuser/Superuser-3.0.7-d-signed.zip"
->>>>>>> 709922b412684a35369a49b6832bed2219630776
         },
         {
             "name": "su binary-only 3.0.3.2 d",
             "summary": "Donut",
             "device": "all",
 <<<<<<< HEAD
-<<<<<<< HEAD
             "url": "http://downloads.noshufou.netdna-cdn.com/superuser/su-bin-3.0.3.2-d-signed.zip"
 =======
-=======
-            "url": "http://downloads.androidsu.com/superuser/su-bin-3.0.3.2-d-signed.zip"
->>>>>>> 709922b412684a35369a49b6832bed2219630776
         },
         {
             "name": "Superuser 2.3.6.3 efgh",
             "summary": "Eclair, Froyo, Gingerbread, and Honeycomb (no longer supported)",
             "device": "all",
 <<<<<<< HEAD
-<<<<<<< HEAD
             "url": "http://downloads.noshufou.netdna-cdn.com/superuser/su-2.3.6.3-efgh-signed.zip"
 =======
-=======
-            "url": "http://downloads.androidsu.com/superuser/su-2.3.6.3-efgh-signed.zip"
->>>>>>> 709922b412684a35369a49b6832bed2219630776
         },
         {
             "name": "Superuser 2.3.6.3 cd",
             "summary": "Cupcake and Donut (no longer supported)",
             "device": "all",
 <<<<<<< HEAD
-<<<<<<< HEAD
             "url": "http://downloads.noshufou.netdna-cdn.com/superuser/su-2.3.6.3-cd-signed.zip"
 =======
-=======
-            "url": "http://downloads.androidsu.com/superuser/su-2.3.6.3-cd-signed.zip"
->>>>>>> 709922b412684a35369a49b6832bed2219630776
         }
     ]
 }
