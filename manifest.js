5db033774f4f411cac0a8ece6742e562ec4dbc38
Adam Shanks <chainsdd@gmail.com>
2012-03-24 10:09:21 +0000

    Revert "Fix conflict"
    This reverts commit 58d02d53e29b675d66f4f7cb6b4e9871e3ef24f7.

--- manifest.js
+++ manifest.js
@@ -7,37 +7,55 @@
             "name": "Superuser 3.0.7 efghi",
             "summary": "Eclair, Froyo, Gingerbread, Honeycomb, and Ice Cream Sandwich",
             "device": "all",
-            "url": "http://downloads.noshufou.netdna-cdn.com/superuser/Superuser-3.0.7-efghi-signed.zip"
+            "urls": [
+                "http://goo-inside.me/superuser/Superuser-3.0.7-efghi-signed.zip",
+                "http://downloads.noshufou.netdna-cdn.com/superuser/Superuser-3.0.7-efghi-signed.zip"
+            ]
         },
         {
             "name": "su binary-only 3.0.3.2 efghi",
             "summary": "Eclair, Froyo, Gingerbread, Honeycomb, and Ice Cream Sandwich",
             "device": "all",
-            "url": "http://downloads.noshufou.netdna-cdn.com/superuser/su-bin-3.0.3.2-efghi-signed.zip"
+            "urls": [
+                "http://goo-inside.me/superuser/su-bin-3.0.3.2-efghi-signed.zip",
+                "http://downloads.noshufou.netdna-cdn.com/superuser/su-bin-3.0.3.2-efghi-signed.zip"
+            ]
         },
         {
             "name": "Superuser 3.0.7 d",
             "summary": "Donut",
             "device": "all",
-            "url": "http://downloads.noshufou.netdna-cdn.com/superuser/Superuser-3.0.7-d-signed.zip"
+            "urls": [
+                "http://goo-inside.me/superuser/Superuser-3.0.7-d-signed.zip",
+                "http://downloads.noshufou.netdna-cdn.com/superuser/Superuser-3.0.7-d-signed.zip"
+            ]
         },
         {
             "name": "su binary-only 3.0.3.2 d",
             "summary": "Donut",
             "device": "all",
-            "url": "http://downloads.noshufou.netdna-cdn.com/superuser/su-bin-3.0.3.2-d-signed.zip"
+            "urls": [
+                "http://goo-inside.me/superuser/su-bin-3.0.3.2-d-signed.zip",
+                "http://downloads.noshufou.netdna-cdn.com/superuser/su-bin-3.0.3.2-d-signed.zip"
+            ]
         },
         {
             "name": "Superuser 2.3.6.3 efgh",
             "summary": "Eclair, Froyo, Gingerbread, and Honeycomb (no longer supported)",
             "device": "all",
-            "url": "http://downloads.noshufou.netdna-cdn.com/superuser/su-2.3.6.3-efgh-signed.zip"
+            "urls": [
+                "http://goo-inside.me/superuser/su-2.3.6.3-efgh-signed.zip",
+                "http://downloads.noshufou.netdna-cdn.com/superuser/su-2.3.6.3-efgh-signed.zip"
+            ]
         },
         {
             "name": "Superuser 2.3.6.3 cd",
             "summary": "Cupcake and Donut (no longer supported)",
             "device": "all",
-            "url": "http://downloads.noshufou.netdna-cdn.com/superuser/su-2.3.6.3-cd-signed.zip"
+            "urls": [
+                "http://goo-inside.me/superuser/su-2.3.6.3-cd-signed.zip",
+                "http://downloads.noshufou.netdna-cdn.com/superuser/su-2.3.6.3-cd-signed.zip"
+            ]
         }
     ]
 }
