4554965debc813aced27a1740dc2aaaca022c984
Adam Shanks <chainsdd@gmail.com>
2012-05-29 23:38:42 +0100

    Add missing import from last commit

--- src/com/noshufou/android/su/AppDetailsFragment.java
+++ src/com/noshufou/android/su/AppDetailsFragment.java
@@ -31,6 +31,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
